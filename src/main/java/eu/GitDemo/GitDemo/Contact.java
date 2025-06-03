414afda7cef35ee127c8c7f740f9d7efb4737928
laffoo <laffoof@gmail.com>
2025-06-03 16:17:58 +0200

    num tel en int

--- src/main/java/eu/GitDemo/GitDemo/Contact.java
+++ src/main/java/eu/GitDemo/GitDemo/Contact.java
@@ -3,7 +3,7 @@ package eu.GitDemo.GitDemo;
 public class Contact {
 
     private String nom;
-    private String  prenom;
-    private String numeroTelephone;
+    private String prenom;
+    private int numeroTelephone;
     private String adresseMail;
 }
