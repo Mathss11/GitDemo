4b6ea245e2a5d8ecf493ec3a5d0973283e2ba6cf
quentinguillaume <q_guillaume@icloud.com>
2025-06-04 09:42:16 +0200

    ajout du sout.

--- src/main/java/eu/GitDemo/GitDemo/Main.java
+++ src/main/java/eu/GitDemo/GitDemo/Main.java
@@ -3,6 +3,6 @@ package eu.GitDemo.GitDemo;
 public class Main {
     public static void main (String[] args) {
         Contact cestMoi = new Contact("Phillipe","Etchbest", "06 12 34 56 78","phillipe@cauchemar.fr");
-        cestMoi.presentation();
+        System.out.println(cestMoi.presentation());
     }
 }
